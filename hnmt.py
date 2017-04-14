0311461fae7506e1e6b628af854e2da515cb948d
Jörg Tiedemann <jorg.tiedemann@helsinki.fi>
2017-04-14 19:35:58 +0300

    added tokenizer option bpe

--- hnmt.py
+++ hnmt.py
@@ -424,10 +424,10 @@ def main():
             metavar='FILE',
             help='name of target language file')
     parser.add_argument('--source-tokenizer', type=str,
-            choices=('word', 'space', 'char'), default=argparse.SUPPRESS,
+            choices=('word', 'space', 'char', 'bpe'), default=argparse.SUPPRESS,
             help='type of preprocessing for source text')
     parser.add_argument('--target-tokenizer', type=str,
-            choices=('word', 'space', 'char'), default=argparse.SUPPRESS,
+            choices=('word', 'space', 'char', 'bpe'), default=argparse.SUPPRESS,
             help='type of preprocessing for target text')
     parser.add_argument('--max-source-length', type=int,
             metavar='N',
